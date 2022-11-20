1185ab74096d5cedb6cebb456a78f48397e997de
Nedim <nsahovic1@etf.unsa.ba>
2022-11-20 21:47:24 +0100

    Initial commit

---
+++ src/test/java/org/selenium/pom/api/actions/DummyClass.java
@@ -0,0 +1,7 @@
+package org.selenium.pom.api.actions;
+
+public class DummyClass {
+    public static void main(String args[]) {
+        System.out.println(new SignUpApi().fetchRegisterNonceValueUsingJsoup());
+        }
+    }
---
+++ src/test/java/org/selenium/pom/api/actions/SignUpApi.java
@@ -0,0 +1,52 @@
+package org.selenium.pom.api.actions;
+
+import io.restassured.http.Cookies;
+import io.restassured.response.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import org.selenium.pom.utils.ConfigLoader;
+
+
+
+import static io.restassured.RestAssured.given;
+
+public class SignUpApi {
+    private Cookies cookies;
+
+    public Cookies getCookies() {
+        return cookies;
+    }
+
+    public String fetchRegisterNonceValueUsingGroovy(){
+        Response response = getAccount();
+        return response.htmlPath().getString("**.findAll { it.@name == 'woocommerce-register-nonce' }.@value");
+    }
+
+    public String fetchRegisterNonceValueUsingJsoup(){
+        Response response = getAccount();
+        Document doc = Jsoup.parse(response.body().prettyPrint());
+        Element element = doc.selectFirst("#woocommerce-register-nonce");
+        return element.attr("value");
+    }
+
+
+    public Response getAccount() {
+        Cookies cookies = new Cookies();
+        Response response = given().
+                baseUri(ConfigLoader.getInstance().getBaseUrl()).
+                cookies(cookies).
+                log().all().
+                when().
+                get("/account").
+                then().
+                log().all().
+                extract().
+                response();
+        if (response.getStatusCode() != 200) {
+            throw new RuntimeException("Failed to fetch the account, HTTP Status Code: " + response.getStatusCode());
+        }
+        return response;
+    }
+}
